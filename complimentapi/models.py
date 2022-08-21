8472a09dac88e8c2e0237f34e82387214152453f
minty <jhkieha@gmail.com>
2022-08-21 16:15:02 -0400

    adding a user model to store google user profile info

---
+++ complimentapi/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from datetime import datetime
+
+class User(models.Model):
+    email = models.CharField(max_length=255, unique=True)
+    first_name = models.CharField(max_length=255, null=True)
+    last_name = models.CharField(max_length=255, null=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True)
