7296a757be5de4233d897df6645820ba41758102
Henry Kiehna <jhkieha@gmail.com>
2022-08-22 02:07:33 -0400

    adding Receiver serializer

--- complimentapi/serializers.py
+++ complimentapi/serializers.py
@@ -1,8 +1,15 @@
 from rest_framework import serializers
 
-from complimentapi.models import User
+from complimentapi.models import User, Receiver
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
+
+
+class ReceiverSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Receiver
+        fields = '__all__'
+        read_only_fields = ['id, user_id']
