f8297832761a112a84a19291fe0d0d36b14696b8
Henry Kiehna <jhkieha@gmail.com>
2022-08-22 11:13:02 -0400

    misunderstood related_name, removed.

--- complimentapi/models.py
+++ complimentapi/models.py
@@ -29,7 +29,7 @@ class User(models.Model):
         return True
 
 class Receiver(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True, null=True)
     updated_at = models.DateTimeField(auto_now=True, null=True)
