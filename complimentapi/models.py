08877a62fdad5d2838c4c6a14ae644c9d4d1541f
Henry Kiehna <33327590+jhkiehna@users.noreply.github.com>
2022-08-22 00:47:42 -0400

    Feature/auth (#3)
    * removing admin panel and messages middleware
    
    * adding simple jwt auth package
    
    * removing log lines and adding a /me endpoint
    
    * adding some missing properties from the custom user model. Certain middlewares need these here
    
    * adding some settings to enable the jwt auth package and change the default auth user to my custom user
    
    Co-authored-by: Henry Kiehna <jhkieha@gmail.com>

--- complimentapi/models.py
+++ complimentapi/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from datetime import datetime
 
 class User(models.Model):
     email = models.CharField(max_length=255, unique=True)
@@ -7,3 +6,24 @@ class User(models.Model):
     last_name = models.CharField(max_length=255, null=True)
     created_at = models.DateTimeField(auto_now_add=True, null=True)
     updated_at = models.DateTimeField(auto_now=True, null=True)
+
+    is_active = True
+
+    REQUIRED_FIELDS = []
+    USERNAME_FIELD = 'email'
+
+    @property
+    def is_anonymous(self):
+        """
+        Always return False. This is a way of comparing User objects to
+        anonymous users.
+        """
+        return False
+
+    @property
+    def is_authenticated(self):
+        """
+        Always return True. This is a way to tell if the user has been
+        authenticated in templates.
+        """
+        return True
