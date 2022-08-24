4235c2d719b31aab95d17949e673e083294324a2
Henry Kiehna <jhkieha@gmail.com>
2022-08-24 02:31:26 -0400

    adding compliment serializer

--- complimentapi/serializers.py
+++ complimentapi/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from complimentapi.models import User, Receiver
+from complimentapi.models import User, Receiver, Compliment
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -13,4 +13,11 @@ class ReceiverSerializer(serializers.ModelSerializer):
     class Meta:
         model = Receiver
         fields = '__all__'
-        read_only_fields = ['id, user']
+        read_only_fields = ['id, user', 'created_at', 'updated_at']
+
+
+class ComplimentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Compliment
+        fields = '__all__'
+        read_only_fields = ['id, receiver', 'created_at', 'updated_at']
